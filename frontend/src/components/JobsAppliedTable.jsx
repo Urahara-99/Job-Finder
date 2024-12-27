e03cd2b404d251d157e9e328eb9fa1e57ab830d8
Hariharasudhan M <harimhs028@gmail.com>
2024-12-27 23:16:52 +0530

    updating Applied jobs page

--- frontend/src/components/JobsAppliedTable.jsx
+++ frontend/src/components/JobsAppliedTable.jsx
@@ -1,53 +1,49 @@
-import React from 'react'
-import companyIcon from '../assets/accenture_logo.png'
+import React from 'react';
+import companyIcon from '../assets/accenture_logo.png';
 
 const JobsAppliedTable = () => {
   return (
-    <div className='w-full mt-5 justify-center ml-20'>
+    <div className="w-full mt-5 justify-center px-5">
       <div>
-         <h2 className='text-[17px] font-semibold mb-5'>Jobs Applied</h2>
-         <div className="overflow-hidden rounded-lg border border-gray-300">
-  <table className="w-full border-separate border-spacing-x-4 text-left">
-    <thead className="bg-gray-100">
-      <tr className="text-[15px]">
-        <th className="px-4 py-3">Company</th>
-        <th className="px-4 py-3">Job Title</th>
-        <th className="px-4 py-3">Location</th>
-        <th className="px-4 py-3">Date</th>
-        <th className="px-4 py-3">Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr className="text-[15px]">
-        <td className="px-4 py-5 flex items-center gap-2">
-          <img src={companyIcon} alt="Company Logo" className="w-10" />
-          <p>Accenture</p>
-        </td>
-        <td className="px-4 py-5">Fullstack Developer</td>
-        <td className="px-4 py-5">Bangalore</td>
-        <td className="px-4 py-5">22 Aug, 2024</td>
-        <td className="px-4 py-5">Pending</td>
-      </tr>
-      <tr className="text-[15px]">
-        <td className="px-4 py-5 flex items-center gap-2">
-          <img src={companyIcon} alt="Company Logo" className="w-10" />
-          <p>Accenture</p>
-        </td>
-        <td className="px-4 py-5">Fullstack Developer</td>
-        <td className="px-4 py-5">Bangalore</td>
-        <td className="px-4 py-5">22 Aug, 2024</td>
-        <td className="px-4 py-5">Pending</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
-
-
-
-
+        <h2 className="text-[17px] font-semibold mb-5">Jobs Applied</h2>
+        <div className="overflow-x-auto">
+          <table className="w-full md:w-[900px] border-separate border-spacing-x-4 text-left">
+            <thead>
+              <tr className="text-[15px]">
+                <th className="px-4 py-3">Company</th>
+                <th className="px-4 py-3">Job Title</th>
+                <th className="px-4 py-3">Location</th>
+                <th className="px-4 py-3">Date</th>
+                <th className="px-4 py-3">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="text-[14px] border-b border-gray-300">
+                <td className="px-4 py-5 flex items-center gap-2">
+                  <img src={companyIcon} alt="Company Logo" className="w-10" />
+                  <p>Accenture</p>
+                </td>
+                <td className="px-4 py-5">Fullstack Developer</td>
+                <td className="px-4 py-5">Bangalore</td>
+                <td className="px-4 py-5">22 Aug, 2024</td>
+                <td className="px-4 py-5">Pending</td>
+              </tr>
+              <tr className="text-[15px] border-b border-gray-300">
+                <td className="px-4 py-5 flex items-center gap-2">
+                  <img src={companyIcon} alt="Company Logo" className="w-10" />
+                  <p>Google</p>
+                </td>
+                <td className="px-4 py-5">Marketing Manager</td>
+                <td className="px-4 py-5">London</td>
+                <td className="px-4 py-5">15 Oct, 2024</td>
+                <td className="px-4 py-5">Accepted</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default JobsAppliedTable
+export default JobsAppliedTable;
--- frontend/src/components/ResumePart.jsx
+++ frontend/src/components/ResumePart.jsx
@@ -1,37 +1,40 @@
-import React from 'react'
-import uploadIcon from '../assets/upload.png'
+import React from 'react';
+import uploadIcon from '../assets/upload.png';
 
 const ResumePart = () => {
-  
   const [upload, setUpload] = React.useState(false);
 
   function displayUploadIcon() {
-    setUpload(prev=>!prev)
+    setUpload((prev) => !prev);
   }
+
   return (
-    <div className='mt-10 w-full grid-flow-row ml-20'>
-        <div className='w-2/5'>
-            <div>
-                <h2 className='text-[17px] font-semibold'>Your Resume</h2>
-            </div>
-            <div className='py-3 gap-3 flex'>
-                <button className='py-1 px-3 bg-blue-100 text-sky-600 rounded-lg text-sm'>resume</button>
-                <button className='py-1 px-4 bg-slate-100 border border-slate-400 text-slate-500 text-sm rounded-lg'>Edit</button>
-                { upload ? null : 
-                <button className='py-1 px-4 border-sky-500 border-2 rounded-lg'
-                onClick={displayUploadIcon}>
-            <img src={uploadIcon} alt="Upload Icon" className="w-6 h-6" />
-          </button>}
-          { upload ? <input
-                type="file"
-                name='file'
-                src={uploadIcon} alt="Upload Icon"/> 
-              : null }
-          </div>
+    <div className="mt-10 px-5">
+      <div className="w-full md:w-2/5">
+        <div>
+          <h2 className="text-[17px] font-semibold">Your Resume</h2>
         </div>
+        <div className="py-3 gap-3 flex flex-wrap">
+          <button className="py-1 px-3 bg-blue-100 text-sky-600 rounded-lg text-sm">
+            Resume
+          </button>
+          <button className="py-1 px-4 bg-slate-100 border border-slate-400 text-slate-500 text-sm rounded-lg">
+            Edit
+          </button>
+          {!upload ? (
+            <button
+              className="py-1 px-4 border-sky-500 border-2 rounded-lg"
+              onClick={displayUploadIcon}
+            >
+              <img src={uploadIcon} alt="Upload Icon" className="w-6 h-6" />
+            </button>
+          ) : (
+            <input type="file" name="file" alt="Upload Icon" />
+          )}
+        </div>
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default ResumePart
+export default ResumePart;
